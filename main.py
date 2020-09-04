dae5cec0c4d1353cd0f8541b97e5dfa4d198d1fd
Matt Amponah <mattamponsah@gmail.com>
2020-09-04 23:26:48 +0000

    Fixed else error message

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-
-
-
+#Authour Matt Amponsah mma6356@psu.edu
 gp1 = input("Enter your course 1 letter grade: ")
 
 credit1 = float(input("Enter your course 1 credit: "))
@@ -14,11 +12,11 @@ elif gp1 == 'C+' : gp1=float(2.33)
 elif gp1 == 'C' : gp1=float(2.0)
 elif gp1 == 'D' : gp1=float(1.0)
 elif gp1 == 'F' : gp1=float(0.0)      
-else: print(f"Invalid Unit for gp1{gp1}")
+else: print(f"Grade point for course 1 is: {0.0}"
 
 fgp1 = (gp1*credit1)/credit1
 
-print(f"Grade point for course 1 is {fgp1}" )
+print(f"Grade point for course 1 is: {fgp1}" )
 
 
 gp2 = input("Enter your course 2 letter grade: ")
@@ -34,7 +32,7 @@ elif gp2 == 'C+' : gp2=float(2.33)
 elif gp2 == 'C' : gp2=float(2.0)
 elif gp2 == 'D' : gp2=float(1.0)
 elif gp2 == 'F' : gp2=float(0.0)      
-else: print(f"Invalid Unit for gp1{gp1}")
+else: print(f"Grade point for course 2 is: {0.0}")
 
 fgp2 = (gp2*credit2)/credit2
 
@@ -54,7 +52,7 @@ elif gp3 == 'C+' : gp3=float(2.33)
 elif gp3== 'C' : gp3=float(2.0)
 elif gp3 == 'D' : gp3=float(1.0)
 elif gp3 == 'F' : gp3=float(0.0)      
-else: print(f"Invalid Unit for gp1{gp1}")
+else: print(f"Grade point for course 3 is: {0.0}")
 
 fgp3 = (gp3*credit3)/credit3
 
